cf51c39ff15a7ffe64cc8a5f88517d811f90fb2e
Ramon Silveira Dall'Agnol <ramon.dallagnol@aegro.com.br>
2021-07-07 09:31:54 -0300

    feat: use repository into Field controller

--- backend/src/main/java/br/com/aegro/fms/controller/FieldController.java
+++ backend/src/main/java/br/com/aegro/fms/controller/FieldController.java
@@ -1,10 +1,34 @@
 package br.com.aegro.fms.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import br.com.aegro.fms.domain.Field;
+import br.com.aegro.fms.domain.FieldRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/rest/fields")
 public class FieldController {
+
+    private final FieldRepository fieldRepository;
+
+    public FieldController(FieldRepository fieldRepository) {
+        this.fieldRepository = fieldRepository;
+    }
+
+    @GetMapping
+    public List<Field> get() {
+        return fieldRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Field getById(@PathVariable Long id) {
+        return fieldRepository.findById(id).orElse(null);
+    }
+
+    @PostMapping
+    public Field save(@RequestBody Field field) {
+        return fieldRepository.save(field);
+    }
 
 }
